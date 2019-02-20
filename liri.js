317937ad534387953eedde30da4d555e90eab42d
Nate Micinski <nmicinski@gmail.com>
2019-02-19 21:24:03 -0800

    dry up code

--- liri.js
+++ liri.js
@@ -56,21 +56,11 @@ function followUp() {
     });
 }
 
-function spotifySearch(userInput) {
+function getSpotifyInput(userInput) {
+    var input;
     if (userInput) {
-        spotify
-            .search({ type: 'track', query: userInput, limit: 1 })
-            .then(function (response) {
-                console.log("Artist: " + response.tracks.items[0].album.artists[0].name);
-                console.log("Song name: " + response.tracks.items[0].name);
-                console.log("Album name: " + response.tracks.items[0].album.name);
-                console.log("Preview link: " + response.tracks.items[0].preview_url);
-                followUp();
-            })
-            .catch(function () {
-                console.log("Sorry, I couldn't find that song. Please try asking again!");
-                actions("spotify-this-song");
-            });
+        input = userInput;
+        spotifySearch(input);
     } else {
         inquirer.prompt([
             {
@@ -82,36 +72,32 @@ function spotifySearch(userInput) {
             if (!answer.userInput) {
                 answer.userInput = "The Sign, Ace of Base";
             }
-            spotify
-                .search({ type: 'track', query: answer.userInput, limit: 1 })
-                .then(function (response) {
-                    console.log("Artist: " + response.tracks.items[0].album.artists[0].name);
-                    console.log("Song name: " + response.tracks.items[0].name);
-                    console.log("Album name: " + response.tracks.items[0].album.name);
-                    console.log("Preview link: " + response.tracks.items[0].preview_url);
-                    followUp();
-                })
-                .catch(function () {
-                    console.log("Sorry, I couldn't find that song. Please try asking again!");
-                    actions("spotify-this-song");
-                });
+            spotifySearch(answer.userInput);
         });
     }
 }
 
-function concertSearch(userInput) {
+function spotifySearch(input) {
+    spotify
+        .search({ type: 'track', query: input, limit: 1 })
+        .then(response =>  {
+            console.log("Artist: " + response.tracks.items[0].album.artists[0].name);
+            console.log("Song name: " + response.tracks.items[0].name);
+            console.log("Album name: " + response.tracks.items[0].album.name);
+            console.log("Preview link: " + response.tracks.items[0].preview_url);
+            followUp();
+        })
+        .catch(function () {
+            console.log("Sorry, I couldn't find that song. Please try asking again!");
+            actions("spotify-this-song");
+        });
+}
+
+function getConcertInput(userInput) {
+    var input;
     if (userInput) {
-        axios.get("https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp")
-            .then(function (response) {
-                console.log("Venue name: " + response.data[0].venue.name);
-                console.log("Venue location: " + response.data[0].venue.city + ", " + response.data[0].venue.country);
-                console.log("Event date: " + moment(response.data[0].datetime).format("MM/DD/YYYY"));
-                followUp();
-            })
-            .catch(function () {
-                console.log("That band isn't touring right now. Try moshing to another band!");
-                actions("concert-this");
-            });
+        input = userInput;
+        concertSearch(input);
     } else {
         inquirer.prompt([
             {
@@ -120,39 +106,30 @@ function concertSearch(userInput) {
                 name: "userInput"
             }
         ]).then(answer => {
-            axios.get("https://rest.bandsintown.com/artists/" + answer.userInput + "/events?app_id=codingbootcamp")
-                .then(function (response) {
-                    console.log("Venue name: " + response.data[0].venue.name);
-                    console.log("Venue location: " + response.data[0].venue.city + ", " + response.data[0].venue.country);
-                    console.log("Event date: " + moment(response.data[0].datetime).format("MM/DD/YYYY"));
-                    followUp();
-                })
-                .catch(function () {
-                    console.log("That band isn't touring right now. Try moshing to another band!");
-                    actions("concert-this");
-                });
+            concertSearch(answer.userInput);
         });
     }
 }
 
-function movieSearch(userInput) {
+function concertSearch(input) {
+    axios.get("https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp")
+        .then(function (response) {
+            console.log("Venue name: " + response.data[0].venue.name);
+            console.log("Venue location: " + response.data[0].venue.city + ", " + response.data[0].venue.country);
+            console.log("Event date: " + moment(response.data[0].datetime).format("MM/DD/YYYY"));
+            followUp();
+        })
+        .catch(function () {
+            console.log("That band isn't touring right now. Try moshing to another band!");
+            actions("concert-this");
+        });
+}
+
+function getMovieInput(userInput) {
+    var input;
     if (userInput) {
-        axios.get("http://www.omdbapi.com/?apikey=7a69e743&t=" + userInput)
-            .then(function (response) {
-                console.log("Title: " + response.data.Title);
-                console.log("Release year: " + response.data.Year);
-                console.log("IMDB Rating: " + response.data.Ratings[0].Value);
-                console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
-                console.log("Production Country: " + response.data.Country);
-                console.log("Language: " + response.data.Language);
-                console.log("Actors: " + response.data.Actors);
-                console.log("Plot: " + response.data.Plot);
-                followUp();
-            })
-            .catch(function () {
-                console.log("This isn't the movie you're looking for... Try searching for something else!");
-                actions("movie-this");
-            });
+        input = userInput;
+        movieSearch(input);
     } else {
         inquirer.prompt([
             {
@@ -161,27 +138,27 @@ function movieSearch(userInput) {
                 name: "userInput"
             }
         ]).then(answer => {
-            if (!answer.userInput) {
-                answer.userInput = "Mr. Nobody";
-            }
-            axios.get("http://www.omdbapi.com/?apikey=7a69e743&t=" + answer.userInput)
-                .then(function (response) {
-                    console.log("Title: " + response.data.Title);
-                    console.log("Release year: " + response.data.Year);
-                    console.log("IMDB Rating: " + response.data.Ratings[0].Value);
-                    console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
-                    console.log("Production Country: " + response.data.Country);
-                    console.log("Language: " + response.data.Language);
-                    console.log("Actors: " + response.data.Actors);
-                    console.log("Plot: " + response.data.Plot);
-                    followUp();
-                })
-                .catch(function () {
-                    console.log("This isn't the movie you're looking for... Try searching for something else!");
-                    actions("movie-this");
-                });
+            movieSearch(answer.userInput);
         });
     }
+}
+
+function movieSearch(input) {
+    axios.get("http://www.omdbapi.com/?apikey=7a69e743&t=" + input)
+        .then(function (response) {
+            console.log("Title: " + response.data.Title);
+            console.log("Release year: " + response.data.Year);
+            console.log("IMDB Rating: " + response.data.Ratings[0].Value);
+            console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+            console.log("Production Country: " + response.data.Country);
+            console.log("Language: " + response.data.Language);
+            console.log("Actors: " + response.data.Actors);
+            console.log("Plot: " + response.data.Plot);
+            followUp();
+        }).catch(function () {
+            console.log("This isn't the movie you're looking for... Try searching for something else!");
+            actions("movie-this");
+        });
 }
 
 function doWhatItSays() {
@@ -206,13 +183,13 @@ Switch
 function actions(command, userInput) {
     switch (command) {
         case "spotify-this-song":
-            spotifySearch(userInput);
+            getSpotifyInput(userInput);
             break;
         case "concert-this":
-            concertSearch(userInput);
+            getConcertInput(userInput);
             break;
         case "movie-this":
-            movieSearch(userInput);
+            getMovieInput(userInput);
             break;
         case "do-what-it-says":
             doWhatItSays();
